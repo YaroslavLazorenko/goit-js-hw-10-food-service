b1302a5b192a7caad76d9afdb98431a38b570257
YaroslavLazorenko <lazorenko85@gmail.com>
2021-09-27 02:46:45 +0300

    Adding template, refs and markup by template

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import './styles.css';
-import './menu.json';
+import './js/storage';
+import data from './db/menu.json';
+import template from './templates/template.hbs';
+import refs from './js/refs';
+
+const { list } = refs;
+const markup = template(data);
+list.insertAdjacentHTML('beforeend', markup);
